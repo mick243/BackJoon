3485cd35b9abfb8eef968dae015284749ccfb92b
mick243 <39043252+mick243@users.noreply.github.com>
2025-06-13 12:05:25 +0900

    Create 21598.js

---
+++ 21598.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const n = Number(input[0]);
+
+for (let i = 0; i < n; i++) {
+  console.log("SciComLove");
+}

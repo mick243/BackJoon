cb09358c2f46d82c21ded7e183f8d2247c2369c1
mick243 <39043252+mick243@users.noreply.github.com>
2025-06-16 13:11:56 +0900

    Create 10814.js

---
+++ 10814.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const age = input.shift();
+
+input.sort((a,b) => a.split(" ")[0] - b.split(" ")[0]);
+
+console.log(input.join("\n"));

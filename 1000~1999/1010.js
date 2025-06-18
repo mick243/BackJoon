c7254281cc06682cf9fc502365b2a878766cb6b2
mick243 <39043252+mick243@users.noreply.github.com>
2025-06-18 15:07:00 +0900

    Update 1010.js

--- 1000~1999/1010.js
+++ 1000~1999/1010.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "C:/Users/hjara/js/example.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let T =  parseInt(input[0]);
 let index = 1;

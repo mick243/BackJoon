df271e3049a1ce1e961120deba6ed9c9d9976ddc
mick243 <39043252+mick243@users.noreply.github.com>
2025-07-07 17:41:20 +0900

    Create 11726.js

---
+++ 11726.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const dp = {
+  1: 1,
+  2: 2,
+};
+
+for (let i = 3; i <= input; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[input]);
